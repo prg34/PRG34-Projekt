f2aa1c585b5335454bc2de365a4fa1780c76970d
prg34 <torsten.bosecker@gmail.com>
2014-12-23 13:01:47 +0100

    erste Implementierungen + kleine Änderungen am Klassenmodell

--- game/src/de/fherfurt/prg34/World.java
+++ game/src/de/fherfurt/prg34/World.java
@@ -1,7 +1,39 @@
 package de.fherfurt.prg34;
 
+import java.util.ArrayList;
+
 /**
- * Created by torsten on 23.12.14.
+ * contains and manages all objects in the (game-)world
  */
 public class World {
+    public World() {
+    }
+
+    public void update() {
+    }
+
+    public void draw() {
+    }
+
+    public void addItem(Item item) {
+    }
+
+    public Item removeItem(string itemname) {
+
+        return item;
+    }
+
+    public void addObject(Object object) {
+    }
+
+    public void addCharacter(Character character) {
+    }
+
+    private int xPos;
+    private int yPos;
+    //bufferedImage array
+    private Player player;
+    private ArrayList<Item> itemList;
+    private ArrayList<Object> objectList;
+    private ArrayList<Character> characterList;
 }
