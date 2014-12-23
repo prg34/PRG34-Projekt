09b47d674fd30c010d190c09d3ea2081293e3578
prg34 <torsten.bosecker@gmail.com>
2014-12-23 09:31:19 +0100

    test2

--- game/src/de/fherfurt/prg34/Game.java
+++ game/src/de/fherfurt/prg34/Game.java
@@ -2,9 +2,10 @@ package de.fherfurt.prg34;
 
 /**
  * Created by torsten on 23.12.14.
+ * to be implemented
  */
 public class Game {
     public static void main(String[] args) {
-        System.out.println("Hi!");
+
     }
 }
