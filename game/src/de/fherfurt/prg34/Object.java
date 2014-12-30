9b56431868a980a4c6dfbbf1c05af410c0002ae4
pe9250we <peter.weigand@fh-erfurt.de>
2014-12-30 18:33:00 +0100

    test3000

--- game/src/de/fherfurt/prg34/Object.java
+++ game/src/de/fherfurt/prg34/Object.java
@@ -1,7 +1,26 @@
 package de.fherfurt.prg34;
 
 /**
- * Created by torsten on 23.12.14.
+ * The objects contained in the world, also not usable ones.
  */
+
 public class Object {
+    public Object(String name, int xPos, int yPos) {
+        this.name = name;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        sizeX = 20;
+        sizeY = 20;
+    }
+
+    public void draw() {
+    }
+
+    private String name;
+    private int xPos;
+    private int yPos;
+    private int sizeX;
+    private int sizeY;
+
+    //bufferedImage
 }
