562d3842ca0cef712df990bd93ac2d18c048084b
pe9250we <peter.weigand@fh-erfurt.de>
2015-01-18 17:45:50 +0100

    update ct

--- game/src/de/fherfurt/prg34/CharacterTest.java
+++ game/src/de/fherfurt/prg34/CharacterTest.java
@@ -6,17 +6,23 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CharacterTest {
 
     @Test
-    public void testReceiveItemFromPlayer() throws Exception {
+    public void testReceiveItemFromPlayer() throws Exception { /*relevant? 18.01*/
 
     }
 
     @Test
     public void testGiveItemToPlayer() throws Exception {
+        Item ash = new Item("Asche", "Was das wohl mal war?", null, null, 15, 15, false);
+        Character Niklas = new Character("Niklas", 15, 15, "Ich habe Asche für dich", ash);                                 /*wegen string []*/
 
+        assertEquals(
+                "After the method giveItemToPlayer, the Item is given.",
+                Niklas.giveItemToPlayer(ash));
     }
 
 }
